251b25f8979e17287270cda12f0632445d37865a
NicksPatties <nickspatties@gmail.com>
2024-04-02 10:33:08 -0700

    Change site icon to a burger. Adjust site title.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "🍔 NicksPatties";
+export const SITE_TITLE = "NicksPatties";
 export const SITE_DESCRIPTION = "NicksPatties: A Nick Patti Portfolio";
