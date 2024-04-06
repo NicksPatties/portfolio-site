6dc908b4ec6903023fb53195e2803f6de1d30cef
NicksPatties <nickspatties@gmail.com>
2024-04-05 19:46:02 -0700

    Added rudimentary autolink content to the page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,28 @@
 import { defineConfig } from "astro/config";
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkToc from "remark-toc";
+
+/**
+  @see https://github.com/rehypejs/rehype-autolink-headings?tab=readme-ov-file#options
+*/
+const autolinkOptions = {
+  behavior: 'append',
+  content: {
+    type: 'element',
+    tagName: 'span',
+    properties: {
+      className: 'haha'
+    },
+    children: [{ type: 'text', value: 'a' }]
+  },
+  properties: {
+    ariaHidden: true
+  }
+
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +30,10 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
   markdown: {
     syntaxHighlight: "prism",
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, autolinkOptions]
+    ]
   }
 });
