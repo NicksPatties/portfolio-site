f2f6b109ca5276857acbe242f4b428c1035debc0
NicksPatties <nickspatties@gmail.com>
2024-04-10 18:29:57 -0700

    Update social media meta tags. Add home screen card.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "NicksPatties";
-export const SITE_DESCRIPTION = "NicksPatties: A Nick Patti Portfolio";
+export const SITE_DESCRIPTION = "A Nick Patti Portfolio";
