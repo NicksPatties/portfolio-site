c146feec0346143f2bd36e0183e497fb8438fbf0
NicksPatties <nickspatties@gmail.com>
2024-04-30 14:11:29 -0700

    Fix path alias configuration. Add utility function to get all posts.

---
+++ src/utils/posts.ts
@@ -0,0 +1,13 @@
+import { type CollectionEntry, getCollection } from "astro:content";
+
+type Blog = CollectionEntry<"blog">
+
+/**
+  @returns all my blog posts, sorted by most recent to least recent.
+**/
+export default async function getPosts(): Promise<Blog[]> {
+  const isDev = import.meta.env.MODE === "development";
+  return (await getCollection("blog", (post: Blog) => isDev || (!isDev && post.data.published)
+))
+  .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+}
